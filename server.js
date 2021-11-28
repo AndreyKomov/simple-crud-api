ea95fe8be6f88303d53ea46c253cd26f07fde44c
Andrei Komau <andrei_komau@epam.com>
2021-11-28 13:26:43 +0300

    feat: add router and persons file

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 require('dotenv').config();
 const http = require("http");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
+const options = {
+    // path: '/person',
+    path: '/',
+    method: 'GET',
+}
+
+const req = http.request(options, (res) => {
+	let data = ''
+	
+	res.on('data', (chunk) => {
+		data += chunk;
+	});
+	
+	res.on('end', () => {
+		console.log('Body:', JSON.parse(data))
+	});
+	
+}).on("error", (err) => {
+	console.log("Error: ", err)
+}).end();
 
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    apiGet();
+/*       switch (req.method) {
+          case 'GET':
+
+      }
+      console.log(req.url); */
+
+    // res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("respons from service");
   })
-  .listen(port, () => {
-    console.log("server is running");
-  });
+  .listen(port);
